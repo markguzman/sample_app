565be45cfb9fe84c909d011f44e9606b3f3ff0c2
markguzman <mark@mty.ai>
2017-07-19 17:40:23 -0700

    Push

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/...'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
-
+  get  'static_pages/home'
+  get  'static_pages/help'
   root 'application#hello'
 end
